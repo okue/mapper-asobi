3a9cb8773a345a7998e13164a350b74398a7b2c7
okue <nogideca@gmail.com>
2019-11-02 13:37:03 +0900

    update

---
+++ src/main/java/Empty2.java
@@ -0,0 +1,5 @@
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+public class Empty2 {
+}
--- src/main/kotlin/Play.kt
+++ src/main/kotlin/Play.kt
@@ -7,8 +7,16 @@ fun <T> writeValueAsString(x: T) = objectMapper.writeValueAsString(x).let { prin
 data class Data(val x: List<FlexMessage>)
 data class Data2(val x: List<Any>)
 data class Data3<T>(val x: List<T>)
+class Empty
 
 fun main() {
+    objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+    objectMapper.writeValueAsString(Empty()).let { println(it) }
+    objectMapper.writeValueAsString(Empty2()).let { println(it) }
+    objectMapper.writeValueAsString(Empty1()).let { println(it) }
+}
+
+fun f1() {
     val m = FlexMessage.builder().build()
     val lst: List<FlexMessage> = listOf(m)
     writeValueAsString(m)
